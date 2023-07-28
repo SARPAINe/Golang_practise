3e5e32b2b7d46046600d65bdcf3e195851f60084
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-07-28 11:58:11 +0600

    section 4 of structs completed

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,56 @@
 package main
 
-type person struct{
-	firstName string
-	lastName string
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
-func main(){
-	 
+type person struct {
+	firstName string
+	lastName  string
+	// contact   contactInfo
+	contactInfo
+	//we can also write just contactInfo
 }
+
+func main() {
+	//  alex:=person{"Alex","Benjamin"}
+	shaharin := person{firstName: "Shaharin", lastName: "Ahmed"}
+	var reyad person
+	reyad.firstName = "Shakil"
+	reyad.lastName = "Ahmed"
+	//  fmt.Println(alex.firstName,shaharin.lastName)
+	shaharin.lastName = "Kazi"
+	fmt.Println(shaharin)
+	fmt.Printf("%+v\n", reyad)
+
+	jim := person{firstName: "Jim", lastName: "Collins", contactInfo: contactInfo{email: "jim@gmail.com", zipCode: 940}}
+	jim.print()
+	jim.updateName("Shawn")
+	jim.print()
+
+	message := []string{"hi", "there"}
+	fmt.Println(message)
+	updateMessage(message)
+	fmt.Println(message)
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func updateMessage(m []string) {
+	m[0] = "bye"
+}
+
+// int, float, string, bool and structs are value types
+// use pointers to change these in a function
+// slices, maps, channels, pointers, functions inside functions
+// these are reference types no need to use pointer.
