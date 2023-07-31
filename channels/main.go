c57a2fdc115000390de229be7d8da8240b9e16a6
Shaharin Ahmed <ahmedshaharin7589@gmail.com>
2023-07-31 22:47:35 +0600

    course follow along compmleted, stil so many confusion, need to do projects

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,17 +21,34 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<-c)
+	// for i:=0;i<len(links);i++{
+	// 	fmt.Println(<-c)
+	// }
+
+	//infinite loop syntax
+	// for{
+	// 	go checkLink(<-c,c)
+	// }
+
+	//alternative infinite loop
+	// 
+	for l:=range c{
+		// go checkLink(l,c)
+		go func(link string){
+			time.Sleep(5*time.Second)
+			checkLink(link,c)
+		}(l)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up."
+	c <- link
 }
